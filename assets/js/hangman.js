4bef77151d305f96ec6f1b9244ce9197f6b374e3
Karina Titov <karinasul1995@gmail.com>
2020-02-13 16:20:11 -0600

    added a bit of css and started js

---
+++ assets/js/hangman.js
@@ -0,0 +1,10 @@
+// Array
+var words = ["Russia", "China", "France", "Japan", "Germany"];
+// Pick a word
+var wordChosen = Math.floor(Math.random() * words.length);
+// underscore = word length
+// User guess
+// Check if its right
+// If right > right array
+// If wrong > wrong array
+
