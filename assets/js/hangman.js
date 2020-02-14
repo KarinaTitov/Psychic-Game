9c1a5d1aa861dc397ffdf039c37048e500c93326
Karina Titov <karinasul1995@gmail.com>
2020-02-14 04:57:30 -0600

    can't make it work

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -1,10 +1,56 @@
 // Array
 var words = ["Russia", "China", "France", "Japan", "Germany"];
+
 // Pick a word
-var wordChosen = Math.floor(Math.random() * words.length);
+var wordChosen = words[Math.floor(Math.random() * words.length)];
+var underScore = [];
+var rightWords = [];
+var wrongWords = [];
+
+
+
+ var docUnderScore = document.getElementById("underscore");
+ var docRightGuess = document.getElementById("rightguess");
+ var docWrongGuess = document.getElementById("wrongguess");
+ 
+ 
+ console.log(wordChosen);
+
+
 // underscore = word length
+var genUnderScores = () => {
+    for (var i = 0; i < wordChosen.length; i++ ) {
+    underScore.push ("_");
+}
+    return underScore;
+}
+
+ 
 // User guess
-// Check if its right
-// If right > right array
-// If wrong > wrong array
+document.addEventListener("keypress", (event) => {
+   var keycode = event.keyCode;
+   var keyWord = String.fromCharCode(keycode);
+   // If right > right array
+   if(wordChosen.indexOf(keyWord) > -1) {
+    rightWords.push(keyWord);
+    // Replace the underscore with the letter
+    underscore[wordChosen.indexOf(keyWord)] = keyWord;
+    docUnderScore[0].innerHTML = underscore.join(" ");
+    docRightGuess[0].innerHTML = rightWords;
+    
+  if(underScore.join("") == wordChosen) {
+       alert("You Winner!!!");
+   }  
+}
+   // If wrong > wrong array
+   else wrongWords.push(keyWord);
+  docWrongGuess[0].innerHTML = wrongWords;
+
+});
+
+docUnderScore[0].innerHTML = genUnderScores().join(" ");
+
+
+
+
 
